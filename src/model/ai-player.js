64b95d751c620211d3d57862c026059c20b7624a
caglaralkiss <caglaralkiss@gmail.com>
2020-04-15 02:07:10 +0300

    feat: add ai-player

---
+++ src/model/ai-player.js
@@ -0,0 +1,30 @@
+(function (window) {
+	'use strict';
+
+	var Player = window.app.Model.Player;
+	var AI = window.app.Util.AI;
+
+	/**
+	 * @augments Player
+	 * @constructor
+	 */
+	function AIPlayer(id, name, mark, score, gameLevel) {
+		Player.call(this, id, name, mark, score);
+
+		// Difficulty level of game.
+		this.gameLevel = gameLevel;
+	}
+
+	AIPlayer.prototype = Object.create(Player.prototype);
+	AIPlayer.prototype.constructor = AIPlayer;
+
+	/**
+	 * Find best possible move from board current state.
+	 *
+	 * @param {Board} board
+	 * @return {*}
+	 */
+	AIPlayer.prototype.findBestMove = function (board) {
+		return AI.findOptimalMove(board, this.mark, this.gameLevel);
+	}
+})(window);
