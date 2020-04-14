997c74499e6839026bb2f011aa21547a48895387
caglaralkiss <caglaralkiss@gmail.com>
2020-04-14 18:24:44 +0300

    feat: implement dom helpers
    * add colors.js to define colors as constant

---
+++ src/constants/colors.js
@@ -0,0 +1,16 @@
+(function (window) {
+
+	/**
+	 * Enumeration of the color in different formats.
+	 *
+	 * @readonly
+	 * @enum {string}
+	 */
+	var COLORS = {
+		BOARD_LINE_COLOR: '#ddd'
+	};
+
+	window.app = window.app || {};
+	window.app.Constants = window.app.Constants || {};
+	window.app.Constants.COLORS = COLORS
+})(window);
---
+++ src/util/dom.js
@@ -0,0 +1,47 @@
+(function (window) {
+
+	/**
+	 * Find the first Element in a given scope or in document when scope is not clarified.
+	 *
+	 * @param {string} selector Refer the selector of the query.
+	 * @param scope
+	 * @return {Element}
+	 */
+	window.qs = function (selector, scope) {
+		return (scope || document).querySelector(selector);
+	};
+
+	/**
+	 * Bind an event listener to a target with given callback function.
+	 *
+	 * @param {Element} target Target element
+	 * @param {string} type Event type
+	 * @param {function} callback
+	 * @param useCapture
+	 */
+	window.$on = function (target, type, callback, useCapture) {
+		target.addEventListener(type, callback, !!useCapture);
+	};
+
+	/**
+	 * Calculate the width of the browser.
+	 *
+	 * @return {number} width
+	 */
+	window.getBrowserWidth = function () {
+		return window.innerWidth
+			|| document.documentElement.clientWidth
+			|| document.body.clientWidth;
+	};
+
+	/**
+	 * Calculate the height of the browser
+	 *
+	 * @return {number} height
+	 */
+	window.getBrowserHeight = function () {
+		return window.innerHeight
+		|| document.documentElement.clientHeight
+		|| document.body.clientHeight;
+	}
+})(window);
