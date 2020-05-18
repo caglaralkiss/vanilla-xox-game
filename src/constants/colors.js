aa6498783297653cdfd89677b3ace9f8dccefff7
caglaralkiss <caglaralkiss@gmail.com>
2020-05-19 02:02:32 +0300

    feat: arrange bg-color of Player/Opponent on runtime

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -9,9 +9,9 @@
 	var COLORS = {
 		WHITE: '#fff',
 		BLACK: '#000',
-		BOARD_LINE_COLOR: '#ddd',
-		X_MARK_COLOR: '#ffa500',
-		O_MARK_COLOR: '#007a7a'
+		BOARD_LINE_COLOR: 'aliceblue',
+		X_MARK_COLOR: '#F15946',
+		O_MARK_COLOR: '#F9C22E'
 	};
 
 	window.app = window.app || {};
--- src/view/player-view.js
+++ src/view/player-view.js
@@ -1,6 +1,8 @@
 (function (window) {
 	'use strict';
 
+	var COLORS = window.app.Constants.COLORS;
+
 	/**
 	 * View that includes Player/Opponent DOM bindings.
 	 * It includes getters & setters for Player template.
@@ -10,6 +12,7 @@
 	 * @constructor
 	 */
 	function PlayerView(playerModel, isOpponent) {
+		this.$root = qs(isOpponent ? '#opponent' : '#player');
 		this.$name = qs((isOpponent ? '#opponent' : '#player') +  ' .player__name');
 		this.$mark = qs((isOpponent ? '#opponent' : '#player') +  ' .player__mark');
 		this.$score = qs((isOpponent ? '#opponent' : '#player') +  ' .player__score');
@@ -30,6 +33,16 @@
 	}
 
 	PlayerView.prototype = {
+		/**
+		 * @param {SQUARE_STATE} mark
+		 */
+		setBackgroundColor: function (mark) {
+			if (mark === 'X') {
+				this.$root.style.backgroundColor = COLORS.X_MARK_COLOR;
+			} else {
+				this.$root.style.backgroundColor = COLORS.O_MARK_COLOR;
+			}
+		},
 		/**
 		 * @param {string} name
 		 */
